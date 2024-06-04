a647032e97e5cb3d968c8208aea837793c35db0e
Jed Kutai <jedkutai@gmail.com>
2024-06-04 07:07:26 -0400

    Initial Commit

---
+++ idomyownstunts/idomyownstuntsApp.swift
@@ -0,0 +1,17 @@
+//
+//  idomyownstuntsApp.swift
+//  idomyownstunts
+//
+//  Created by Jed Kutai on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct idomyownstuntsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
